2019f4a537e6997652df9b5d05217501a8cb625c
Carlos Eduardo Rodrigues dos Santos <carlos.rodrigues@recife.pe.gov.br>
2024-11-26 15:46:15 -0300

    projeto de tarefas concluido

--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-
+from datetime import date
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name="Título", max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(null=False, blank=False)
+    deadline = models.DateField(verbose_name="Data de Entrega", null=False, blank=False)
     finishe_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_has_complete(self):
+        if not self.finishe_at:
+            self.finishe_at = date.today()
+            self.save()
--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Todo
-
-# def todo_list(request):
-#     todos = Todo.objects.all()
-#     return render(request, "todos/todo_list.html", {"todos": todos})
 
 class TodolistView(ListView):
     model = Todo
@@ -14,4 +10,20 @@ class TodolistView(ListView):
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list") #Depois do cadastro redireciona a pagina para essa rota.
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
